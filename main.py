8514957cf702ec2fd4aa8e67c71425937fe2d326
ManojC0 <94547054+ManojC0@users.noreply.github.com>
2024-09-07 11:49:18 +0100

    Added game screen class

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import time
 import random
 import pygame
+import pygame.freetype
 
+pygame.init()
+GAME_FONT = pygame.freetype.Font(None, 24)
+screen_x = 1000
+screen_y = 1000
+screen = pygame.display.set_mode((screen_x,screen_y))
 
 
 class Snake:
@@ -10,6 +16,7 @@ class Snake:
         self.snake_body = [[]]
         self.current_direction = "RIGHT"
         self.create_snake(starting_pos)
+        self.score = 0
 
     def create_snake(self, starting_pos):
         self.snake_body = [[starting_pos[0], starting_pos[1]],
@@ -20,7 +27,7 @@ class Snake:
 
     def draw(self):
         for position in self.snake_body:
-            pygame.draw.rect(screen, pygame.Color(255, 255, 255), pygame.Rect(position[0], position[1], 10, 10))
+            pygame.draw.rect(screen, pygame.Color(0, 255, 0), pygame.Rect(position[0], position[1], 10, 10))
 
 
 
@@ -67,6 +74,7 @@ class Snake:
         # Append the new segment to the snake's body
         self.snake_body.append(new_segment)
 
+
 class Fruit:
     def __init__(self, screen_rect):
         self.screen_rect = screen_rect
@@ -79,8 +87,8 @@ class Fruit:
                                                (self.screen_rect["y"] + self.screen_rect["h"] - 10)) // 10) * 10]
 
     def draw(self):
-        print(self.fruit_position)
-        pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(
+        #print(self.fruit_position)
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(
             self.fruit_position[0], self.fruit_position[1], 10, 10))
 
     # Return the current position of the fruit
@@ -96,21 +104,54 @@ class Fruit:
         self.set_position()
 
 
-pygame.init()
+class GameScreen:
+    def __init__(self,left_corner,screen_dim):
+        self.screen_dim = screen_dim
+        self.left_corner = left_corner
+        self.screen_rect_dict = {}
+       # self.screen_rect = 0
+        self.middle_of_screen = (0, 0)
+        self.create_screen_rect_dict()
+        self.mid_screen()
+       # screen = pygame.display.set_mode([screen_dim[0zzzz], screen_height])
+
+    def create_screen(self):
+        return pygame.Rect(self.screen_rect_dict["x"], self.screen_rect_dict["y"],
+                    self.screen_rect_dict["w"], self.screen_rect_dict["h"])
+
+    def create_screen_rect_dict(self):
+        self.screen_rect_dict = {"x": int(self.left_corner[0]), "y": int(self.left_corner[1]),
+                                 "w": int(self.screen_dim[0]), "h": int(self.screen_dim[1])}
+
+    def mid_screen(self):
+        self.middle_of_screen = (self.screen_rect_dict["x"] + self.screen_rect_dict["w"] / 2,
+                                 self.screen_rect_dict["y"] + self.screen_rect_dict["h"] / 2)
+
+    def create_grid(self):
+        for column in range(self.screen_rect_dict["x"]+10, self.screen_rect_dict["x"]+ self.screen_rect_dict["w"], 10):
+            pygame.draw.line(screen,(255,255,255),(column,self.screen_rect_dict["y"]),
+                             (column,self.screen_rect_dict["y"] + self.screen_rect_dict["h"]))
+
+        for row in range(self.screen_rect_dict["y"]+10, self.screen_rect_dict["y"] + self.screen_rect_dict["h"], 10):
+            pygame.draw.line(screen, (255, 255, 255), (self.screen_rect_dict["x"], row),
+                             (self.screen_rect_dict["x"] + self.screen_rect_dict["w"], row))
+
+
 snake_speed = 10
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
-screen_x = 1000
-screen_y = 1000
-
+human_screen = GameScreen([650,400], [300,300])
+robot_screen = GameScreen([300,400], [300,300])
 screen = pygame.display.set_mode((screen_x,screen_y))
 screen.fill((0, 0, 0))
 
-human_screen_rect_dict = {"x": screen_x/4, "y": screen_y/4, "w": 600, "h": 600}  # (x,y) refer to top left corner, (w,h) are width and height
-human_screen_mid = (human_screen_rect_dict["x"]+human_screen_rect_dict["w"]/2,
-                    human_screen_rect_dict["y"]+human_screen_rect_dict["h"]/2)  # calculates mid point for player snake spawn
-human_game_screen = pygame.Rect(human_screen_rect_dict["x"], human_screen_rect_dict["y"],
-                                human_screen_rect_dict["w"], human_screen_rect_dict["h"])
-
+#human_screen_rect_dict = {"x": screen_x/4, "y": screen_y/4, "w": 600, "h": 600}  # (x,y) refer to top left corner, (w,h) are width and height
+#human_screen_mid = (human_screen_rect_dict["x"]+human_screen_rect_dict["w"]/2,
+ #                   human_screen_rect_dict["y"]+human_screen_rect_dict["h"]/2)  # calculates mid point for player snake spawn
+# = pygame.Rect(human_screen_rect_dict["x"], human_screen_rect_dict["y"],
+ #                             human_screen_rect_dict["w"], human_screen_rect_dict["h"])
+#print(human_game_screen)
 
 pygame.display.set_caption("Snake")
 current_direction = "RIGHT"
@@ -120,19 +161,15 @@ run = True
 fps = pygame.time.Clock()
 
 # Snake position
-snake = Snake(human_screen_mid)
+snake = Snake(human_screen.middle_of_screen)
+snake_bot = Snake(robot_screen.middle_of_screen)
 
-# snake_head = [human_screen_mid[0], human_screen_mid[1]]
-# snake = [  [human_screen_mid[0], human_screen_mid[1]],
-#            [human_screen_mid[0] -10, human_screen_mid[1]],
-#            [human_screen_mid[0] -20, human_screen_mid[1]],
-#            [human_screen_mid[0] -30, human_screen_mid[1]],
-#        ]
 
 # fruit position
 # fruit_position = [(random.uniform(human_screen_rect_dict["x"], human_screen_rect_dict["x"]+human_screen_rect_dict["w"]-10)//10)*10,
 #                   (random.uniform(human_screen_rect_dict["y"], (human_screen_rect_dict["y"]+human_screen_rect_dict["h"]-10))//10)*10]
-fruit = Fruit(human_screen_rect_dict)
+fruit = Fruit(human_screen.screen_rect_dict)
+fruit_robot = Fruit(robot_screen.screen_rect_dict)
 
 fruit_spawn = True
 score = 0
@@ -144,15 +181,14 @@ def game_over():
 
     return False
 
-
-pygame.draw.rect(screen, pygame.Color(255, 0, 255), human_game_screen,width=1)
+#print(human_screen.create_screen())
+pygame.draw.rect(screen, pygame.Color(255, 0, 255), human_screen.create_screen(),width=1)
+human_screen.create_grid()
 snake.draw()
 pygame.display.update()
 
 fruit.set_position()
 while run:
-
-
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
@@ -163,35 +199,42 @@ while run:
                 direction = "RIGHT"
             if event.key == pygame.K_LEFT:
                 direction = "LEFT"
-    #print(snake.snake_body)
+
     snake.valid_direction(direction)
     snake.movement()
 
     if fruit.check_collision(snake.snake_head):
        snake.extend()
+       snake.score += 50
        fruit.respawn()
 
-    screen.fill((0, 0, 0),human_game_screen)
+    screen.fill((0, 0, 0),human_screen.create_screen())
 
     # draw snake
     snake.draw()
+
     # draw fruit
     fruit.draw()
 
     # draw human screen
-    pygame.draw.rect(screen, pygame.Color(255, 0, 255), human_game_screen, width=1)
+    human_screen.create_grid()
+    pygame.draw.rect(screen, pygame.Color(255, 0, 255), human_screen.create_screen(), width=1)
 
-
-    if snake.snake_head[0] < human_screen_rect_dict["x"] or snake.snake_head[0] == human_screen_rect_dict["x"]+human_screen_rect_dict["w"]: # The conditional operators here are because the display updates act funny
-        print(snake.snake_head)
-        print(human_screen_rect_dict["x"],human_screen_rect_dict["y"])
+    if snake.snake_head[0] < human_screen.screen_rect_dict["x"] or snake.snake_head[0] == \
+            human_screen.screen_rect_dict["x"]+human_screen.screen_rect_dict["w"]: # The conditional operators here are because the display updates act funny
+        #print(snake.snake_head)
+        #print(human_screen_rect_dict["x"],human_screen_rect_dict["y"])
         run =  game_over()
-    if snake.snake_head[1] < human_screen_rect_dict["y"] or snake.snake_head[1] == human_screen_rect_dict["y"]+human_screen_rect_dict["h"]:
+    if snake.snake_head[1] < human_screen.screen_rect_dict["y"] or snake.snake_head[1] == \
+            human_screen.screen_rect_dict["y"]+human_screen.screen_rect_dict["h"]:
         run = game_over()
 
     for body in snake.snake_body[1:]:
         if snake.snake_head[0] == body[0] and snake.snake_head[1] == body[1]:
-            game_over()
+            run = game_over()
+    snake.score += 1
     pygame.display.update()
     fps.tick(snake_speed)
+
+print(snake.score)
 pygame.quit()
